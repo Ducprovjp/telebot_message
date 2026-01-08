e57e41cb8ec6bfbc7cabcb53fe634619a6402b60
Ducprovjp <ducprovjp2612@gmail.com>
2026-01-08 13:42:26 +0700

    start service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegramModule } from './telegram/telegram.module';
 import { ConfigModule } from '@nestjs/config';
+import { DiscordModule } from './discord/discord.module';
+import { MessageHandlerModule } from './shared/message-handler/message-handler.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TelegramModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TelegramModule,
+    DiscordModule,
+    MessageHandlerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
